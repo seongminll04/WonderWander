f2fceb2fdea65c0938b49dfed9845836f5a8ffe2
DobroL33 <dobrolee26@gmail.com>
2024-01-11 02:56:20 +0900

    [WOW-35-FE-RankingPageScroll] 스크롤 페이지

--- FrontEnd/src/pages/ranking.tsx
+++ FrontEnd/src/pages/ranking.tsx
@@ -1,21 +1,69 @@
 import {
-    StyleSheet,
-    View,
-    Text,
-  } from "react-native";
-import React from "react";
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  SafeAreaView,
+  TextInput,
+  FlatList,
+} from 'react-native';
+import React, {useState} from 'react';
 
-
-  
 function Ranking() {
-    return (
-      <View style={{ flex: 1 }}>
-        <Text>랭킹</Text>
+  const [isFriend, setIsFriend] = useState(false);
+  const toggleSwitch = () => setIsFriend(!isFriend);
+  return (
+    <View style={{flex: 1, backgroundColor: 'white'}}>
+      <View style={styles.toggle}>
+        <TouchableOpacity
+          style={!isFriend ? styles.buttonOn : styles.buttonOff}
+          onPress={toggleSwitch}>
+          <Text>전체</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={!isFriend ? styles.buttonOff : styles.buttonOn}
+          onPress={toggleSwitch}>
+          <Text>친구</Text>
+        </TouchableOpacity>
       </View>
-    );
-  }
+    </View>
+  );
+}
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    // marginHorizontal: '6%',
+  },
+  toggle: {
+    width: '70%',
+    height: 60,
+    marginLeft: '15%',
+    marginTop: '10%',
+    borderRadius: 25,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 5,
+    backgroundColor: '#F8F6FC',
+  },
+  buttonOn: {
+    width: '50%',
+    height: 50,
+    backgroundColor: '#ffffff',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonOff: {
+    width: '50%',
+    height: 50,
+    backgroundColor: '#F8F6FC',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
 export default Ranking;
-  
